3d346fd6614190a7c3791a4b83e2f01a1da65ab8
JErnestoHc <julmaster15@gmail.com>
2023-03-24 16:31:06 -0600

    modificando nombre de variable

--- ATM/src/main/java/Atm/funciones/Operaciones.java
+++ ATM/src/main/java/Atm/funciones/Operaciones.java
@@ -2,7 +2,7 @@ package Atm.funciones;
 
 public class Operaciones {
     private float totalCordobas;
-    private float todalDolares;
+    private float totalDolares;
 
     public Operaciones() {
     }
@@ -11,20 +11,20 @@ public class Operaciones {
         return totalCordobas;
     }
 
-    public float getTodalDolares() {
-        return todalDolares;
+    public float getTotalDolares() {
+        return totalDolares;
     }
 
     public void guardarMonto(TIPOMONEDA moneda, float monto) {
-        if (moneda == TIPOMONEDA.DOLARES) this.todalDolares += monto;
+        if (moneda == TIPOMONEDA.DOLARES) this.totalDolares += monto;
         totalCordobas += monto;
     }
 
     public void retirar(TIPOMONEDA tipomoneda, float monto) {
-        if (compararMonto(monto, todalDolares)) {
+        if (compararMonto(monto, totalDolares)) {
             if (tipomoneda == TIPOMONEDA.DOLARES) {
 
-                todalDolares -= monto;
+                totalDolares -= monto;
                 System.out.println("Se retiro:" + monto);
                 mostrarMensaje(tipomoneda);
             }else {
@@ -40,7 +40,7 @@ public class Operaciones {
 
     public String consultarSaldo(TIPOMONEDA tipomoneda) {
         if (tipomoneda == TIPOMONEDA.DOLARES) {
-            return TIPOMONEDA.DOLARES.getMoneda() + getTodalDolares();
+            return TIPOMONEDA.DOLARES.getMoneda() + getTotalDolares();
         }
         return TIPOMONEDA.CORDOBAS.getMoneda() + getTotalCordobas();
     }
@@ -54,7 +54,7 @@ public class Operaciones {
 
     private void mostrarMensaje(TIPOMONEDA tipomoneda){
         if (tipomoneda==TIPOMONEDA.DOLARES){
-            System.out.println("Saldo actual:" + getTodalDolares() + TIPOMONEDA.DOLARES);
+            System.out.println("Saldo actual:" + getTotalDolares() + TIPOMONEDA.DOLARES);
         }else {
             System.out.println("Saldo actual:" + getTotalCordobas() + TIPOMONEDA.CORDOBAS);
         }
