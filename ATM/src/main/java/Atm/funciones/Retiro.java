ecda1ae2892c2ba3e0d8a3f65cc935510d714db5
Schavez0497 <schavezmoraga@gmail.com>
2023-03-24 17:15:07 -0600

    Agregando lectura de retiro

--- ATM/src/main/java/Atm/funciones/Retiro.java
+++ ATM/src/main/java/Atm/funciones/Retiro.java
@@ -1,4 +1,9 @@
 package Atm.funciones;
 
 public class Retiro {
+    public void retirar (TIPOMONEDA tipomoneda,float monto) {
+        Operaciones operaciones = new Operaciones();
+        operaciones.retirar(tipomoneda,monto);
+        System.out.println("Su retiro de:" + monto + "exitoso.");
+    }
 }
