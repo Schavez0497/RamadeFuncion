08c41d0cb5f0e54f7b088780b7fefbbb529eaea1
JErnestoHc <calerojulio92@gmail.com>
2023-03-25 07:41:19 -0600

    Modificando la implementacion para usar el patron de diseño singleton

--- ATM/src/main/java/Atm/funciones/Retiro.java
+++ ATM/src/main/java/Atm/funciones/Retiro.java
@@ -2,7 +2,7 @@ package Atm.funciones;
 
 public class Retiro {
     public void retirar (TIPOMONEDA tipomoneda,float monto) {
-        Operaciones operaciones = new Operaciones();
+        Operaciones operaciones = Operaciones.NewOperacion();
         operaciones.retirar(tipomoneda,monto);
         System.out.println("Su retiro de:" + monto + "exitoso.");
     }
